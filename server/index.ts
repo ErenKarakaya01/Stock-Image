9cc64559f5c3731e47008e88b4fe5351c277b437
ErenKarakaya <erenkarakaya@eskisehir.edu.tr>
2022-05-02 20:59:55 +0300

    mysql connection problems solved

--- server/index.ts
+++ server/index.ts
@@ -9,41 +9,49 @@ const mysql = require("mysql")
 
 const db_config = {
   host: "eu-cdbr-west-02.cleardb.net",
-  user: "bd57c0dda4db27",
-  password: "43d943f3",
-  database: "heroku_19042e25087c122",
+  user: "b1bc500c66092e",
+  password: "e868a98a",
+  database: "heroku_39f5874a1a20466",
 }
 
 server.prepare().then(() => {
   const app = express()
 
-  let db = mysql.createConnection(db_config)
-  /* db.on("connection", function (connection: any) {
-    console.log("Connected to ClearDB!")
+  let connection: any
 
+  function handleDisconnect() {
+    connection = mysql.createConnection(db_config) 
+    // Recreate the connection, since
+    // the old one cannot be reused.
+
+    connection.connect(function (err: any) {
+      // The server is either down
+      if (err) {
+        // or restarting (takes a while sometimes).
+        console.log("error when connecting to db:", err)
+        setTimeout(handleDisconnect, 2000) 
+        // We introduce a delay before attempting to reconnect,
+      } // to avoid a hot loop, and to allow our node script to
+    }) 
+    // process asynchronous requests in the meantime.
+    // If you're also serving http, display a 503 error.
     connection.on("error", function (err: any) {
-      console.error(new Date(), "MySQL error", err.code)
+      console.log("db error", err)
+      if (err.code === "PROTOCOL_CONNECTION_LOST") {
+        // Connection to the MySQL server is usually
+        handleDisconnect() // lost due to either server restart, or a
+      } else {
+        // connnection idle timeout (the wait_timeout
+        // server variable configures this)
+        throw err
+      }
     })
-    connection.on("close", function (err: any) {
-      console.error(new Date(), "MySQL close", err)
-    })
-  })
-  db.query("select 1", (err: string, rows: object[]) => {
+  }
+
+  handleDisconnect()
+  connection.query("select 1", (err: string, rows: object[]) => {
     if (err) throw err
     console.log(rows)
-  }) */
-
-  db.connect((err: any) => {
-    if (err) throw err
-
-    console.log("Connected to ClearDB!")
-    
-    setInterval(function () {
-      db.query("SELECT 1", (err: any, rows: object[]) => {
-        if (err) throw err
-        console.log(rows)
-      })
-    }, 5000)
   })
 
   app.all("*", (req: any, res: any) => {
